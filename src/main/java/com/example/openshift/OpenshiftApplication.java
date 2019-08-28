4b4777efe71f57a783f09eb76b19e396c01bb42d
AnilPatel <anil_patel@toyota.co>
2019-08-28 13:00:32 +0530

    rest api

--- src/main/java/com/example/openshift/OpenshiftApplication.java
+++ src/main/java/com/example/openshift/OpenshiftApplication.java
@@ -2,9 +2,17 @@ package com.example.openshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OpenshiftApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Hello openshift ...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftApplication.class, args);

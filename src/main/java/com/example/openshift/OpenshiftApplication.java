ebe1585be57679faad94bc05a5c119245b370209
AnilPatel <anil_patel@toyota.co>
2019-08-28 12:52:29 +0530

    new files

---
+++ src/main/java/com/example/openshift/OpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftApplication.class, args);
+	}
+
+}
